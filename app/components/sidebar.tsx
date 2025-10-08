ab40e3afdf6990b9cb76c06337fda446e7db5d91
Md Yusuf Ansari <mdyusuf1202@gmail.com>
2025-10-08 23:59:03 +0530

    Start Learning The Next Js Basic To Advance

---
+++ app/components/sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar({currentPath = "/dashboard"}:{currentPath: string}) {
+  return (
+    <div className=''>
+
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Sidebar from "../components/sidebar";
+
+export default function DashboardPage() {
+    return (
+        <div>
+            <Sidebar currentPath="/dashboard"/>
+        </div>
+    )
+}
