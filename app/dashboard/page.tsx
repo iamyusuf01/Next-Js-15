728f6cb9ae2c6a68d655bc4872c0f94e144fc910
Md Yusuf Ansari <mdyusuf1202@gmail.com>
2025-10-14 01:17:28 +0530

    Sidebar Completed and Dashboard in progress

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,34 @@
 import Sidebar from "../components/sidebar";
+import { getCurrentUser } from "../lib/auth";
+import { prisma } from "../lib/prisma";
 
-export default function DashboardPage() {
-    return (
-        <div>
-            <Sidebar currentPath="/dashboard"/>
+export default async function DashboardPage() {
+  const user = await getCurrentUser();
+  const userId = user.id;
+
+  const totalProduct = await prisma.product.count({ where: { userId } });
+  console.log(totalProduct);
+  
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Sidebar currentPath="/dashboard" />
+      <main className="ml-64 p-8">
+        {/* {Header} */}
+        <div className="mb-8">
+          <div className="flex items-center justify-between">
+            <div>
+              <h1 className="text-2xl font-semibold text-gray-800">
+                Dashboard
+              </h1>
+              <p className="text-sm to-gray-500">
+                Welcome back! Here is an overview of your inventory
+              </p>
+            </div>
+          </div>
         </div>
-    )
+
+        {/* Key Matrics */}
+      </main>
+    </div>
+  );
 }
